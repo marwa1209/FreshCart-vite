ad1afcec8b0551ba804aee5a0f3adfdff605c045
Marwa Mahmoud <mm3764453@gmail.com>
2024-05-29 21:00:25 +0300

    use alternate solution to get last element in infinite scroll

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,10 +1,9 @@
 /** @format */
 
-import { FC, useEffect } from "react";
+import { FC, useCallback, useRef } from "react";
 import { Link } from "react-router-dom";
 import PriceFormat from "../PriceFormat/PriceFormat";
 import Loader from "../Loader/Loader";
-import { useInView } from "react-intersection-observer";
 import { useProducts } from "@/hooks/use-products";
 interface ProductsProps {}
 
@@ -27,12 +26,20 @@ const Products: FC<ProductsProps> = () => {
     isFetchingNextPage,
     hasNextPage,
   } = useProducts();
-  const { ref, inView } = useInView();
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
-    }
-  }, [inView, hasNextPage, fetchNextPage]);
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (isPending) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [fetchNextPage, isPending, hasNextPage]
+  );
 
   if (isPending) {
     return (
@@ -51,50 +58,62 @@ const Products: FC<ProductsProps> = () => {
       </div>
     );
   }
-  if (data?.pages[0]?.data.length > 0) {
+  if (data?.pages[0]?.data?.length > 0) {
     return (
       <div className="my-16 container m-auto">
         <h2>OUR Products</h2>
         {data.pages.map((page: any, pageIndex: number) => (
           <div key={pageIndex}>
             <div className="grid grid-flow-row gap-4 grid-cols-1 xl:grid-cols-3 sm:grid-cols-2 ">
-              {page.data.map((product: product) => (
-                <div
-                  key={product.id}
-                  ref={ref}
-                  className="aspect-square transition-all animate-fadeIn"
-                >
-                  <Link to={"/details/" + product.id}>
-                    <div className="group flex flex-col h-full w-full items-center justify-center overflow-hidden rounded-lg border hover:border-blue-600 relative ">
-                      <img
-                        className="object-contain w-full h-full transition duration-300 ease-in-out group-hover:scale-105"
-                        src={product.imageCover}
-                        alt={product.title}
-                      />
-                      <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4 ">
-                        <div className="flex items-center rounded-full border bg-white/70 p-1 text-xs font-semibold text-black">
-                          <h3 className="me-4 pl-2">
-                            {product.title.split(" ").slice(0, 2).join(" ")}{" "}
-                            {product.category.name
-                              .split(" ")
-                              .slice(0, 2)
-                              .join(" ")}
-                          </h3>
-                          <p className="rounded-full bg-blue-600 p-2 text-white">
-                            <PriceFormat price={product.price} />
-                          </p>
+              {page.data.map((product: product, index: number) => {
+                if (
+                  data.pages.length - 1 === pageIndex &&
+                  page.data.length - 1 === index
+                ) {
+                  return (
+                    <div
+                      key={product.id}
+                      className="aspect-square transition-all animate-fadeIn"
+                    >
+                      <Link to={"/details/" + product.id}>
+                        <div
+                          className="group flex flex-col h-full w-full items-center justify-center overflow-hidden rounded-lg border hover:border-blue-600 relative "
+                          ref={lastElementRef}
+                        >
+                          <img
+                            className="object-contain w-full h-full transition duration-300 ease-in-out group-hover:scale-105"
+                            src={product.imageCover}
+                            alt={product.title}
+                          />
+                          <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4 ">
+                            <div className="flex items-center rounded-full border bg-white/70 p-1 text-xs font-semibold text-black">
+                              <h3 className="me-4 pl-2">
+                                {product.title.split(" ").slice(0, 2).join(" ")}{" "}
+                                {product.category.name
+                                  .split(" ")
+                                  .slice(0, 2)
+                                  .join(" ")}
+                              </h3>
+                              <p className="rounded-full bg-blue-600 p-2 text-white">
+                                <PriceFormat price={product.price} />
+                              </p>
+                            </div>
+                          </div>
                         </div>
-                      </div>
+                      </Link>
                     </div>
-                  </Link>
-                </div>
-              ))}
+                  );
+                }
+
+              })}
             </div>
           </div>
         ))}
 
         {isFetchingNextPage && (
-          <i className="fa-solid fa-circle-notch fa-spin me-2"></i>
+          <div className="mt-3 flex justify-center">
+            <i className="fa-solid fa-circle-notch fa-spin me-2 mt-5 h-4 animate-spin"></i>
+          </div>
         )}
       </div>
     );
