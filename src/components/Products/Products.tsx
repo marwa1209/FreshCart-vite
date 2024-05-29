c38746cdd144585410dee26cee74019a5edb38a1
Marwa Mahmoud <mm3764453@gmail.com>
2024-05-29 21:14:19 +0300

    product card view on mobile

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -60,12 +60,13 @@ const Products: FC<ProductsProps> = () => {
   }
   if (data?.pages[0]?.data?.length > 0) {
     return (
-      <div className="my-16 container m-auto">
+      <div className="sm:my-16 m-auto">
         <h2>OUR Products</h2>
         {data.pages.map((page: any, pageIndex: number) => (
           <div key={pageIndex}>
             <div className="grid grid-flow-row gap-4 grid-cols-1 xl:grid-cols-3 sm:grid-cols-2 ">
               {page.data.map((product: product, index: number) => {
+                //if lastElement
                 if (
                   data.pages.length - 1 === pageIndex &&
                   page.data.length - 1 === index
@@ -86,7 +87,7 @@ const Products: FC<ProductsProps> = () => {
                             alt={product.title}
                           />
                           <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4 ">
-                            <div className="flex items-center rounded-full border bg-white/70 p-1 text-xs font-semibold text-black">
+                            <div className="flex-col m-auto items-start rounded-md flex sm:items-center  border bg-white/70 p-1 text-xs font-semibold text-black">
                               <h3 className="me-4 pl-2">
                                 {product.title.split(" ").slice(0, 2).join(" ")}{" "}
                                 {product.category.name
@@ -104,7 +105,36 @@ const Products: FC<ProductsProps> = () => {
                     </div>
                   );
                 }
-
+                return (
+                  <div
+                    key={product.id}
+                    className="aspect-square transition-all animate-fadeIn"
+                  >
+                    <Link to={"/details/" + product.id}>
+                      <div className="group flex flex-col h-full w-full items-center justify-center overflow-hidden rounded-lg border hover:border-blue-600 relative ">
+                        <img
+                          className="object-contain w-full h-full transition duration-300 ease-in-out group-hover:scale-105"
+                          src={product.imageCover}
+                          alt={product.title}
+                        />
+                        <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4 ">
+                          <div className="flex-col m-auto items-start rounded-md flex sm:items-center  border bg-white/70 p-1 text-xs font-semibold text-black">
+                            <h3 className="me-4 pl-2 mb-2">
+                              {product.title.split(" ").slice(0, 2).join(" ")}{" "}
+                              {product.category.name
+                                .split(" ")
+                                .slice(0, 2)
+                                .join(" ")}
+                            </h3>
+                            <p className="rounded-full bg-blue-600 p-2 text-white">
+                              <PriceFormat price={product.price} />
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                );
               })}
             </div>
           </div>
